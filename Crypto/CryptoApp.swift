cb29ab8b7b61facf75b86e35f0accaa7547ec989
Sarika <sarika.paghdal1@gmail.com>
2022-05-07 17:37:51 +0200

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Sarika on 07.05.22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
