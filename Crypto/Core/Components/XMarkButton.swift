09e3f0355cfb0a18655e51df99de51416668eed8
Sarika <sarika.paghdal1@gmail.com>
2022-05-24 15:58:43 +0200

    adding portfolio view and custome button for the view

---
+++ Crypto/Core/Components/XMarkButton.swift
@@ -0,0 +1,28 @@
+//
+//  XMarkButton.swift
+//  Crypto
+//
+//  Created by Sarika on 24.05.22.
+//
+
+import SwiftUI
+
+struct XMarkButton: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }, label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+        })
+    }
+}
+
+struct XMarkButton_Previews: PreviewProvider {
+    static var previews: some View {
+        XMarkButton()
+    }
+}
--- Crypto/CryptoApp.swift
+++ Crypto/CryptoApp.swift
@@ -12,6 +12,11 @@ struct CryptoApp: App {
     //all the view of the app will have access to vm var because we are passing in with environmentObject.
     @State private var vm = HomeViewModel()
     
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
